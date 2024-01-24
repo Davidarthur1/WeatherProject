63b9e13bc8ebd3f287c3425cee1986e7ff6c8f4c
Davidarthur1 <122786239+Davidarthur1@users.noreply.github.com>
2024-01-24 11:15:00 +0000

    Initial commit
    n#

---
+++ src/app/weather.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private http: HttpClient) { }
+
+  getweather(city: string, units: string) {
+    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=b61fb5a124d60a4aa658f60350d01da0&units=' + units);
+  }
+}
