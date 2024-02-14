4cc96eb7b215919f7dc3a1874106ee901615d0de
Davidarthur1 <122786239+Davidarthur1@users.noreply.github.com>
2024-02-14 13:10:07 +0000

    committing changes

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -29,42 +29,43 @@ export class WeatherComponent implements OnInit{
    {
    }
   ngOnInit(): void {
-    this.weatherService.getweather(this.city, this.units).subscribe({
+    // this.weatherService.getweather(this.city, this.units).subscribe({
 
-      next:(res) => {
-        console.log(res)
-        this.myWeather = res;
-        console.log(this.myWeather);
-        this.temperature = this.myWeather.main.temp;
-        this.feelsLikeTemp = this.myWeather.main.feels_like;
-        this.humidity = this.myWeather.main.humidity;
-        this.windspeed = this.myWeather.wind.speed;
-        this.summary = this.myWeather.weather[0].main;
-        this.iconURL = 'https://openweathermap.org/img/wn/' + this.myWeather.weather[0].icon + '@2x.png';
-      },
+    //   next:(res) => {
+    //     console.log(res)
+    //     this.myWeather = res;
+    //     console.log(this.myWeather);
+    //     this.temperature = this.myWeather.main.temp;
+    //     this.feelsLikeTemp = this.myWeather.main.feels_like;
+    //     this.humidity = this.myWeather.main.humidity;
+    //     this.windspeed = this.myWeather.wind.speed;
+    //     this.summary = this.myWeather.weather[0].main;
+    //     this.iconURL = 'https://openweathermap.org/img/wn/' + this.myWeather.weather[0].icon + '@2x.png';
+    //   },
 
-      error: (error) => console.log(error.message),
+    //   error: (error) => console.log(error.message),
 
-      complete: () => console.info('API call completed')
-    })
+    //   complete: () => console.info('API call completed')
+    // })
   }
 
 ['onButtonClick'](cityName:string){
-  this.fiveDayForecastApiResponse = this.weatherService.fivedayforecast(cityName).subscribe({
+  console.log(cityName);
+  this.weatherService.fivedayforecast(cityName).subscribe({
 
     next:(res) => {
       console.log(res)
-      this.myWeather = res;
+      this.fiveDayForecastApiResponse = res;
       console.log(this.myWeather);
-      this.temperature = this.myWeather.main.temp;
-      this.feelsLikeTemp = this.myWeather.main.feels_like;
-      this.humidity = this.myWeather.main.humidity;
-      this.windspeed = this.myWeather.wind.speed;
-      this.summary = this.myWeather.weather[0].main;
-      this.iconURL = 'https://openweathermap.org/img/wn/' + this.myWeather.weather[0].icon + '@2x.png';
-      this.city = cityName;
-      this.lat = this.myWeather.city.coord.lat;
-      this.lon = this.myWeather.city.coord.lon;
+      // this.temperature = this.myWeather.main.temp;
+      // this.feelsLikeTemp = this.myWeather.main.feels_like;
+      // this.humidity = this.myWeather.main.humidity;
+      // this.windspeed = this.myWeather.wind.speed;
+      // this.summary = this.myWeather.weather[0].main;
+      // this.iconURL = 'https://openweathermap.org/img/wn/' + this.myWeather.weather[0].icon + '@2x.png';
+      // this.city = cityName;
+      // this.lat = this.myWeather.city.coord.lat;
+      // this.lon = this.myWeather.city.coord.lon;
     },
 
     error: (error) => console.log(error.message),
@@ -76,9 +77,9 @@ export class WeatherComponent implements OnInit{
 readonly cities: string[] = ['London', 'Cardiff', 'Birmingham'];
 
 
-fiveDayForecast(){
+// fiveDayForecast(){
   
-      return this.http.get('http://api.openweathermap.org/data/2.5/forecast?lat=cityMap.city.lat&lon=cityMap.city.lon&appid=b61fb5a124d60a4aa658f60350d01da0}',)
-    }
-  }
-
+//       return this.http.get('http://api.openweathermap.org/data/2.5/forecast?lat=cityMap.city.lat&lon=cityMap.city.lon&appid=b61fb5a124d60a4aa658f60350d01da0}',)
+//     }
+//   
+}
